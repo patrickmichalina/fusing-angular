18c19de1c1c8a06f1c60cccb25aab82575c0a7c6
Patrick Michalina <patrickmichalina@mac.com>
2019-05-26 15:19:04 -0500

    progress

--- fuse.ts
+++ fuse.ts
@@ -2,101 +2,115 @@ import { FuseBox, QuantumPlugin, WebIndexPlugin } from "fuse-box"
 import { NgCompilerPlugin } from "./tools/plugins/ng.compiler.plugin"
 import { NgPolyfillPlugin } from "./tools/plugins/ng.polyfill.plugin"
 import { CompressionPlugin } from "./tools/plugins/compression.plugin"
-import { NgProdPlugin } from "./tools/plugins/ng.prod.plugin"
 import { NgAotFactoryPlugin } from "./tools/plugins/ng.aot-factory.plugin"
+import { NgProdPlugin } from "./tools/plugins/ng.prod.plugin"
 
 export interface IBundleOptions {
   readonly name: string
+  readonly inputPath: string
+  readonly outputPath: string
+  readonly ignoredModules: string[]
 }
 
-export interface IBundleable {
-  readonly bundle: Partial<IBundleOptions>
+export interface IBundleable<T extends Partial<IBundleOptions> | Required<IBundleOptions> = Partial<IBundleOptions>> {
+  readonly bundle: T
 }
 
-export interface IRequiredBundleable extends IBundleable {
-  readonly bundle: Required<IBundleOptions>
-}
+type IRequiredBundleable = IBundleable<Required<IBundleOptions>>
 
 export interface IEnableable {
   readonly enabled: boolean
 }
 
 export interface IUniversalServerOptions extends IBundleable, IEnableable { }
-export interface IRequiredUniversalServerOptions extends IRequiredBundleable, IEnableable { }
 export interface IElectronOptions extends IBundleable, IEnableable { }
+export interface IRequiredUniversalServerOptions extends IRequiredBundleable, IEnableable { }
 export interface IRequiredElectronOptions extends IRequiredBundleable, IEnableable { }
+
+interface BaseBrowserOptions {
+  readonly supportIE11: boolean
+  readonly supportIE11Animations: boolean
+}
+
+export interface IBrowserOptions extends BaseBrowserOptions, IBundleable { }
+export interface IRequiredBrowserOptions extends BaseBrowserOptions, IRequiredBundleable { }
 
 export interface IProductionBuildOptions extends IEnableable {
   readonly minify: boolean
   readonly treeshake: boolean
 }
 
-export interface FusingAngularConfig {
-  readonly supportIE11: boolean
-  readonly supportIE11Animations: boolean
+export interface BaseOptions {
   readonly enableAotCompilaton: boolean
   readonly enableAngularAnimations: boolean
   readonly enableAngularForms: boolean
   readonly enableServiceWorker: boolean
   readonly enableAngularBuildOptimizer: boolean
-
   readonly watch: boolean
-  readonly homeDir: string
-  readonly serverSrcDir: string
+  readonly srcRoot: string
   readonly browserSrcDir: string
-  readonly outputDir: string
+  readonly outputDirectory: string
   readonly vendorBundleName: string
-  readonly appBundleName: string
-  readonly browserEntry: string
   readonly browserAotEntry: string
   readonly jsOutputDir: string
   readonly jsLazyModuleDir: string
   readonly serve: boolean
-
-
+  readonly browser: Partial<IBrowserOptions>
   readonly electron: Partial<IElectronOptions>
   readonly universal: Partial<IUniversalServerOptions>
   readonly optimizations: Partial<IProductionBuildOptions>
 }
 
-type UserOptions = Partial<FusingAngularConfig>
+type PartialOptions = Partial<BaseOptions>
 
-interface RequiredFusingAngularConfig extends FusingAngularConfig {
+interface Options extends BaseOptions {
+  readonly browser: Required<IRequiredBrowserOptions>
   readonly electron: Required<IRequiredElectronOptions>
   readonly universal: Required<IRequiredUniversalServerOptions>
   readonly optimizations: Required<IProductionBuildOptions>
 }
 
-const DEFAULT_CONFIG: RequiredFusingAngularConfig = {
+const DEFAULT_CONFIG: Options = {
   enableAotCompilaton: false,
-  supportIE11: true,
-  supportIE11Animations: false,
   enableAngularAnimations: false,
   enableAngularForms: false,
   enableServiceWorker: false,
   enableAngularBuildOptimizer: false,
   watch: false,
   serve: false,
-  homeDir: 'src',
-  outputDir: '.dist',
+  srcRoot: 'src',
+  outputDirectory: '.dist',
   jsOutputDir: 'js',
   jsLazyModuleDir: 'modules',
-  serverSrcDir: 'server',
   browserSrcDir: 'browser',
   vendorBundleName: 'vendor',
-  appBundleName: 'app',
-  browserEntry: 'main',
-  browserAotEntry: 'main.aot',
+  browserAotEntry: 'main.aot.ts',
+  browser: {
+    supportIE11: false,
+    supportIE11Animations: false,
+    bundle: {
+      name: 'app',
+      inputPath: 'main.ts',
+      outputPath: 'public/js',
+      ignoredModules: []
+    }
+  },
   electron: {
     enabled: false,
     bundle: {
-      name: ''
+      name: 'electron',
+      inputPath: '',
+      outputPath: '.',
+      ignoredModules: []
     }
   },
   universal: {
     enabled: true,
     bundle: {
-      name: 'server'
+      name: 'server',
+      inputPath: 'server.ts',
+      outputPath: '/public/js/',
+      ignoredModules: ['express', 'domino', 'express-static-gzip']
     }
   },
   optimizations: {
@@ -106,19 +120,34 @@ const DEFAULT_CONFIG: RequiredFusingAngularConfig = {
   }
 }
 
-
 const mergeOptions =
-  (defaultsOptions: RequiredFusingAngularConfig) =>
-    (incomingOptions: UserOptions): RequiredFusingAngularConfig => ({
+  (defaultsOptions: Options) =>
+    (incomingOptions: PartialOptions): Options => ({
       ...defaultsOptions,
       ...incomingOptions,
+      browser: {
+        ...defaultsOptions.browser,
+        ...incomingOptions.browser as IRequiredBrowserOptions,
+        bundle: {
+          ...defaultsOptions.browser.bundle,
+          ...((incomingOptions.browser || {})).bundle
+        }
+      },
       universal: {
         ...defaultsOptions.universal,
-        ...incomingOptions.universal as IRequiredUniversalServerOptions
+        ...incomingOptions.universal as IRequiredUniversalServerOptions,
+        bundle: {
+          ...defaultsOptions.universal.bundle,
+          ...((incomingOptions.universal || {})).bundle
+        }
       },
       electron: {
         ...defaultsOptions.electron,
         ...incomingOptions.electron as IRequiredElectronOptions,
+        bundle: {
+          ...defaultsOptions.electron.bundle,
+          ...((incomingOptions.electron || {})).bundle
+        }
       },
       optimizations: {
         ...defaultsOptions.optimizations,
@@ -126,24 +155,26 @@ const mergeOptions =
       }
     })
 
-export const fusingAngular = (opts: UserOptions) => {
+export const fusingAngular = (opts: PartialOptions) => {
   const settings = mergeOptions(DEFAULT_CONFIG)(opts)
 
   const shared = {
     sourceMaps: settings.optimizations.enabled,
-    cache: !settings.optimizations.enabled
+    cache: !settings.optimizations.enabled,
+    homeDir: settings.srcRoot,
+    output: `${settings.outputDirectory}/$name.js`
   }
 
   const browser = FuseBox.init({
     ...shared,
-    target: 'browser',
-    homeDir: `${settings.homeDir}/${settings.browserSrcDir}`,
-    output: `${settings.outputDir}/public/js/$name.js`,
+    homeDir: `${shared.homeDir}/${settings.browserSrcDir}`,
+    ignoreModules: settings.browser.bundle.ignoredModules,
+    output: `${settings.outputDirectory}/${settings.browser.bundle.outputPath}/$name.js`,
     plugins: [
       NgAotFactoryPlugin({ enabled: settings.enableAotCompilaton }),
       NgPolyfillPlugin({ isAot: settings.enableAotCompilaton }),
       NgCompilerPlugin({ enabled: settings.enableAotCompilaton }),
-      NgProdPlugin({ enabled: settings.optimizations.enabled, fileTest: settings.browserEntry }),
+      NgProdPlugin({ enabled: settings.optimizations.enabled, fileTest: settings.browser.bundle.inputPath }),
       settings.optimizations.enabled && QuantumPlugin({
         uglify: settings.optimizations.minify,
         treeshake: settings.optimizations.treeshake,
@@ -153,7 +184,7 @@ export const fusingAngular = (opts: UserOptions) => {
       CompressionPlugin({ enabled: settings.optimizations.enabled }),
       WebIndexPlugin({
         path: `${settings.jsOutputDir}`,
-        template: `${settings.homeDir}/${settings.browserSrcDir}/index.html`,
+        template: `${settings.srcRoot}/${settings.browserSrcDir}/index.html`,
         target: '../index.html'
       })
     ]
@@ -162,9 +193,7 @@ export const fusingAngular = (opts: UserOptions) => {
   const server = FuseBox.init({
     ...shared,
     target: 'server',
-    homeDir: settings.homeDir,
-    output: `${settings.outputDir}/$name.js`,
-    ignoreModules: ['express', 'domino', 'express-static-gzip'],
+    ignoreModules: settings.universal.bundle.ignoredModules,
     plugins: [
       NgPolyfillPlugin({ isServer: true, fileTest: /server.ts/ }),
       settings.optimizations.enabled && QuantumPlugin({
@@ -179,73 +208,75 @@ export const fusingAngular = (opts: UserOptions) => {
   const electron = FuseBox.init({
     ...shared,
     target: 'electron',
-    homeDir: `${settings.homeDir}/${'electron'}`,
-    output: `${'.dist'}/$name.js`,
+    // homeDir: `${settings.srcRoot}/${'electron'}`,
+    ignoreModules: settings.electron.bundle.ignoredModules,
     plugins: [
       settings.optimizations.enabled && QuantumPlugin({
         replaceProcessEnv: false,
         uglify: settings.optimizations.minify,
-        bakeApiIntoBundle: 'electron',
+        bakeApiIntoBundle: settings.electron.bundle.name,
         treeshake: settings.optimizations.treeshake
       }) as any
     ]
   })
 
   const mainAppEntry = opts.enableAotCompilaton
-    ? `${settings.browserAotEntry}.ts`
-    : `${settings.browserEntry}.ts`
+    ? `${settings.browserAotEntry}`
+    : `${settings.browser.bundle.inputPath}`
 
   browser
     .bundle(settings.vendorBundleName)
     .instructions(` ~ ${mainAppEntry}`)
 
-  const electronBundle = electron
-    .bundle('electron')
-    .instructions(` > [main.ts]`)
-
   const appBundle = browser
-    .bundle(settings.appBundleName)
+    .bundle(settings.browser.bundle.name)
     .splitConfig({ dest: settings.jsLazyModuleDir, browser: `/${settings.jsOutputDir}/` })
     .instructions(` !> [${mainAppEntry}]`)
 
   const serverBundle = server
     .bundle(settings.universal.bundle.name)
     .splitConfig({ dest: settings.jsLazyModuleDir })
-    .instructions(` > ${settings.serverSrcDir}/server.ts`)
+    .instructions(` > ${settings.universal.bundle.inputPath}`)
     .completed(proc => settings.serve && proc.start())
 
+  const electronBundle = electron
+    .bundle(settings.electron.bundle.name)
+    .instructions(` > ${settings.electron.bundle.inputPath}`)
+
   if (settings.watch) {
-    appBundle.watch(`${settings.homeDir}/**`)
-    serverBundle.watch(`${settings.homeDir}/**`)
-    if (settings.electron.enabled) { electronBundle.watch(`${settings.homeDir}/**`) }
+    const watchDir = `${settings.srcRoot}/**`
+    appBundle.watch(watchDir)
+
+    if (settings.universal.enabled) { serverBundle.watch(watchDir) }
+    if (settings.electron.enabled) { electronBundle.watch(watchDir) }
   }
 
-  browser.run().then(() => {
+  browser.run().then(_ => {
     if (settings.electron.enabled) { electron.run() }
-    server.run()
+    if (settings.universal.enabled) { server.run() }
   })
 }
 
 fusingAngular({
-  serve: true,
+  // serve: true,
   // watch: true,
-  // minify: true,
-  // treeshake: true,
-  // productionBuild: true,
-  optimizations: {
-    enabled: true
-  },
+  // optimizations: {
+  //   enabled: true
+  // },
+  // browser: {},
   // universal: {
+  //   enabled: false,
   //   bundle: {
+  //     // outputPath: ''
   //     // name: ''
   //   }
   // },
-  electron: {
-    enabled: false,
-    // bundle: {
-    //   name: ''
-    // }
-    // bundle: {}
-  },
-  enableAotCompilaton: true
+  // electron: {
+  //   enabled: false,
+  //   // bundle: {
+  //   //   name: ''
+  //   // }
+  //   // bundle: {}
+  // },
+  // enableAotCompilaton: true
 })
