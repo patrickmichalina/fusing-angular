49e9523fa52383b8ccf3039e38b23beb2dad02ca
Patrick Michalina <patrickmichalina@mac.com>
2019-05-29 22:01:59 -0500

    static

--- src/server/server.app.ts
+++ src/server/server.app.ts
@@ -7,21 +7,22 @@ import { AppServerModule } from './server.angular.module'
 const app = express()
 const dir = resolve('dist')
 const publicDir = `${dir}/public`
-const jsDir = `${publicDir}/js`
 const expressStaticGzip = require('express-static-gzip')
 
 app.disable('x-powered-by');
 app.use(cookieParser())
-app.use('/js', expressStaticGzip(jsDir, {
-  enableBrotli: true,
-  orderPreference: ['br', 'gzip'] as ReadonlyArray<string>,
-  // maxAge: config.NODE_DEBUG ? '0' : '7d'
-}))
-
 app.set('view engine', 'html')
 app.set('views', publicDir)
-
 app.engine('html', ngExpressEngine({ bootstrap: AppServerModule }))
+
+app.get('/', (req, res) => res.render('index', { req }))
+
+app.use('/', expressStaticGzip(publicDir, {
+  enableBrotli: true,
+  orderPreference: ['br', 'gzip'] as ReadonlyArray<string>,
+  fallthrough: true
+  // maxAge: config.NODE_DEBUG ? '0' : '7d'
+}))
 
 app.get('*', (req, res) => {
   res.render('index', { req })
