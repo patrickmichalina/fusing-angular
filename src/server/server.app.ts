2293c089b44a150edd7e2d0dd6ad402b0daa6f42
Patrick Michalina <patrickmichalina@mac.com>
2019-06-01 23:57:28 -0500

    cache

--- src/server/server.app.ts
+++ src/server/server.app.ts
@@ -1,6 +1,7 @@
 import * as express from 'express'
 import * as cookies from 'cookie-parser'
 import * as compression from 'compression'
+import * as expeditious from 'express-expeditious'
 import { resolve } from 'path'
 import { ngExpressEngine } from '@nguniversal/express-engine'
 import { AppServerModule } from './angular/server.angular.module'
@@ -10,6 +11,10 @@ const app = express()
 const dir = resolve('dist')
 const publicDir = `${dir}/public`
 const expressStaticGzip = require('express-static-gzip')
+const cache = expeditious({
+  namespace: 'expresscache',
+  defaultTtl: '30 seconds' // TODO: part of IReader config w/ Redis engine
+});
 
 app.disable('x-powered-by')
 app.use(cookies())
@@ -17,13 +22,14 @@ app.set('view engine', 'html')
 app.set('views', publicDir)
 app.engine('html', ngExpressEngine({ bootstrap: AppServerModule }))
 
+app.use(cache)
 app.get('/', compression(), (req, res) => res.render('index', { req }))
 
 app.use('/', expressStaticGzip(publicDir, {
   enableBrotli: true,
   orderPreference: ['br', 'gzip'] as ReadonlyArray<string>,
   fallthrough: true
-  //TODO: maxAge: config.NODE_DEBUG ? '0' : '7d'
+  //TODO: maxAge: config.NODE_DEBUG ? '0' : '7d' // TODO: part of IReader config
 }))
 
 registerApi(app)
