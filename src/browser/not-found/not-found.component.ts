354cc29206b9f47f8f284043f430d31c37529217
Patrick Michalina <patrickmichalina@mac.com>
2019-06-13 01:00:47 -0500

    wip

--- src/browser/not-found/not-found.component.ts
+++ src/browser/not-found/not-found.component.ts
@@ -1,9 +1,10 @@
 import { ChangeDetectionStrategy, Component } from '@angular/core'
 
 @Component({
-  changeDetection: ChangeDetectionStrategy.OnPush,
   selector: 'app-not-found',
-  templateUrl: './not-found.component.html'
+  templateUrl: './not-found.component.html',
+  styleUrls: ['./not-found.component.css'],
+  changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class NotFoundComponent {
 }
