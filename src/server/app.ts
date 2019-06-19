823a455b172abba72aae2a5e08493f24f48aebc3
Patrick Michalina <patrickmichalina@mac.com>
2019-06-18 22:09:55 -0500

    cook

--- src/server/app.ts
+++ src/server/app.ts
@@ -1,5 +1,5 @@
 import * as express from 'express'
-// import * as cookies from 'cookie-parser'
+import * as cookies from 'cookie-parser'
 import * as compression from 'compression'
 import { ngExpressEngine } from '@nguniversal/express-engine'
 import { registerApi } from './api'
@@ -7,23 +7,18 @@ import { reader } from 'typescript-monads'
 import { IConfig } from './config'
 import { join } from 'path'
 
-
-
-
 export const createExpressApplication = reader<IConfig, express.Application>(config => {
   const app = express()
   const publicDir = join(config.DIST_FOLDER, config.WWW_ROOT)
   const expressStaticGzip = require('express-static-gzip')
 
-  // app.use(cookies('test'))
+  app.use(cookies())
   app.disable('x-powered-by')
   app.set('view engine', 'html')
   app.set('views', publicDir)
   app.engine('html', ngExpressEngine({ bootstrap: require('./angular/server.angular.module').AppServerModule }) as any)
 
   const angularRender = (req: express.Request, res: express.Response) => {
-    res.cookie('key1', 123, { sameSite: true })
-    res.cookie('key2', JSON.stringify({ test: 456 }))
     res.render('index', { req, res })
   }
 
