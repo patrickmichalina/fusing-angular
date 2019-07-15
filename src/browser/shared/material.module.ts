bec5a72c9f1512fab061a89c0bf48ae4706a2094
Patrick Michalina <patrickmichalina@mac.com>
2019-07-15 00:31:02 -0500

    wip

--- src/browser/shared/material.module.ts
+++ src/browser/shared/material.module.ts
@@ -1,17 +1,19 @@
 
 import { NgModule } from '@angular/core'
 import { DomSanitizer } from '@angular/platform-browser'
-import { MatIconRegistry } from '@angular/material'
+import { MatIconRegistry, MatIconModule } from '@angular/material'
+import { PlatformService } from './platform.service'
 
 @NgModule({
   imports: [
+    MatIconModule
   ],
   exports: [
+    MatIconModule
   ]
 })
 export class MaterialModule {
-  constructor(matIconRegistry: MatIconRegistry, sanitizer: DomSanitizer) {
-    matIconRegistry
-      .addSvgIconSet(sanitizer.bypassSecurityTrustResourceUrl('./mdi.svg'))
+  constructor(matIconRegistry: MatIconRegistry, sanitizer: DomSanitizer, ps: PlatformService) {
+    matIconRegistry .addSvgIconSet(sanitizer.bypassSecurityTrustResourceUrl('./mdi.svg'))
   }
 }
