8fbb24bd280cc507d65ecf95e92512c60777599c
Patrick Michalina <patrickmichalina@mac.com>
2019-08-24 22:25:08 -0500

    wip

--- fuse.ts
+++ fuse.ts
@@ -1,10 +1,38 @@
 import { argv } from 'yargs'
+import { fusebox, sparky } from 'fuse-box'
 
-const cli = {
-  electron: argv.electron,
-  minify: argv.minify,
-  prod: argv.prod,
-  serve: argv.serve,
-  watch: argv.watch,
-  pwa: argv.pwa
+class BuildContext {
+  minify = argv.minify ? true : false
+  prod = argv.prod ? true : false
+  serve = argv.serve ? true : false
+  watch = argv.watch ? true : false
+  pwa = argv.pwa ? true : false
+  fusebox = {
+    server: fusebox({
+      target: 'server'
+    }),
+    browser: fusebox({
+      target: 'browser'
+    }),
+    electron: fusebox({
+      target: 'electron'
+    })
+  }
 }
+
+const { task, exec, rm } = sparky(BuildContext)
+
+task('assets.copy', ctx => { })
+task('assets.compress', ctx => { })
+
+task('build', ctx => { })
+task('build.dev', ctx => { })
+task('build.dev.browser', ctx => { })
+task('build.dev.server', ctx => { })
+
+task('default', ctx => {
+  rm('dist')
+  rm('ngc')
+  return exec('build')
+  // ctx.fusebox.browser.runDev({})
+ })
