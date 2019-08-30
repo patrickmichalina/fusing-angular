1cc94b8a30f86ad6819e0b6f2ac727f09d53b441
Patrick Michalina <patrickmichalina@mac.com>
2019-08-29 20:23:06 -0500

    resolve

--- src/server/app.ts
+++ src/server/app.ts
@@ -7,12 +7,12 @@ import { ngExpressEngine } from '@nguniversal/express-engine'
 import { registerApi } from './api'
 import { reader } from 'typescript-monads'
 import { IConfig } from './config'
-import { join } from 'path'
+import { resolve } from 'path'
 import { AppServerModule } from './angular/server.angular.module'
 
 export const createExpressApplication = reader<IConfig, express.Application>(config => {
   const app = express()
-  const publicDir = join(config.DIST_FOLDER, config.WWW_ROOT)
+  const publicDir = resolve(config.DIST_FOLDER, config.WWW_ROOT)
   const expressStaticGzip = require('express-static-gzip')
   const pino = require('express-pino-logger')
 
