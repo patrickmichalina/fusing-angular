9397b90b46454c02a5be6680e59936201524c607
Patrick Michalina <patrickmichalina@mac.com>
2019-09-15 13:50:44 -0500

    updater module

--- src/browser/shared/fusing/updater/updater.module.service.ts
+++ src/browser/shared/fusing/updater/updater.module.service.ts
@@ -3,11 +3,11 @@ import { SwUpdate } from '@angular/service-worker'
 import { interval, of } from 'rxjs'
 import { switchMap } from 'rxjs/operators'
 import { maybe } from 'typescript-monads'
-import { UPDATE_INTERVAL } from './updater.tokens'
+import { UPDATER_INTERVAL } from './updater.tokens'
 
 @Injectable()
 export class UpdaterService {
-  constructor(private zone: NgZone, @Inject(UPDATE_INTERVAL) private updateInterval: number,
+  constructor(private zone: NgZone, @Inject(UPDATER_INTERVAL) private updateInterval: number,
     @Optional() private updates?: SwUpdate) { }
 
   listenForUpdates = () =>
